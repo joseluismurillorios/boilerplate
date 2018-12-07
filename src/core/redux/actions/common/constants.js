833cc01752c748246f4bcb92346ac614a5885303
Unknown <jose.murillo@tectijuana.edu.mx>
2018-12-07 03:57:52 -0800

    Preparing gh-page

--- src/core/redux/actions/common/constants.js
+++
@@ -1,9 +0,0 @@
-/**
- * Constants common
- */
-
-/** @constant {string} TOGGLE_NAV */
-export const TOGGLE_NAV = 'core/redux/common/TOGGLE_NAV';
-
-/** @constant {string} TOGGLE_THEME */
-export const TOGGLE_THEME = 'core/redux/common/TOGGLE_THEME';
